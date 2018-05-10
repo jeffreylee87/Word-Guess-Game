1f5912633929e2fdc5cfc5871092d0d8c91bdd07
Jeffrey Lee <jeffreylee@dhcp-v100-062.mobile.uci.edu>
2018-05-10 16:06:15 -0700

    updated game working word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,8 @@ var djNames = ["martin garrix", "armin Van Buren", "tiesto", "david guetta", "ti
 var pickWord = djNames[Math.floor(Math.random() * djNames.length)];
 console.log(pickWord);
 
-var correctGuess = [];
+
+// var correctGuess = [];
 
 var hiddenWord = pickWord.replace(/[a-z]/g, "_");
 console.log(hiddenWord);
@@ -12,62 +13,81 @@ console.log(hiddenWord);
 
 var guessLeft = 5;
 var wins = 0;
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var conLetFor;
 
-function startGame (userGuess){
-    if (userGuess !== -1){
+function startGame(userGuess) {
+    if (userGuess !== -1) {
         document.querySelector("#lineToGuess").innerHTML = "<p>The Word: " + hiddenWord + "</p>";
     }
-
 }
-
 
 function decrease() {
     guessLeft--;
     return "<p>Number of Guesses Remaining: " + guessLeft + "</p>";
-};
+}
 
 function endGame() {
-    if (guessLeft === 0){
+    if (guessLeft === 0) {
         document.querySelector("#guesses").innerHTML = "<p>Number of Guesses Remaining: 0</p>";
         alert("Game is over, loser!!!!");
         location.reload();
     }
 }
-function correctG (){ 
-for (var i in pickWord){
-   if(pickWord.charAt(i) === userGuess){
-    correctGuess.push(pickWord[i]);
-   }
-   
+
+function inputGuess(guess) {
+    var arrayWord = pickWord.split("");
+    for (var i in arrayWord) {
+        if (guess === arrayWord[i]) {
+            var splitHidden = hiddenWord.split("");
+            splitHidden[i] = guess;
+            hiddenWord = splitHidden.join('');
+        }
+    }
+
 }
+
+function setWord() {
+    pickWord = djNames[Math.floor(Math.random() * djNames.length)];
+    hiddenWord = pickWord.replace(/[a-z]/g, "_");
+    document.getElementById("letGuessed").innerText = "";
+    guessLeft = 5;
+    document.querySelector("#guesses").textContent = `Number of Guesses Remaining: ${guessLeft}`;
+
 }
-console.log(correctGuess);
 
+document.onkeyup = function (event) {
 
-    document.onkeyup = function (event) {
+    var userGuess = event.key;
 
-        var userGuess = event.key;
-        startGame(userGuess);
+    startGame(userGuess);
 
-        var html = "<p>The Word: " + hiddenWord + "</p>";
+    var html = "<p>The Word: " + hiddenWord + "</p>";
 
-
-
-
-
+    if (/^[a-z]/.test(userGuess)) {
         if (pickWord.indexOf(userGuess) !== -1) {
             alert("picked correct letter");
-            document.querySelector("#lineToGuess").innerHTML = html;
+            inputGuess(userGuess);
+            document.querySelector("#lineToGuess").innerText = "TheWord: " + hiddenWord;
+            if (pickWord === hiddenWord) {
+                wins++;
+                document.querySelector("#wins").innerText = "Wins: " + wins;
+                alert("You Win!!!!!!");
+                setWord();
+                alert("Hit any letter to take a guess")
+            }
         } else {
             document.getElementById("letGuessed").innerHTML += userGuess;
             document.querySelector("#guesses").innerHTML = decrease();
             endGame();
         }
+    } else {
+        alert("Only press letters, nothing else or else......");
+    }
 
-    };
 
-  
+
+
+
+};
+
+
 
